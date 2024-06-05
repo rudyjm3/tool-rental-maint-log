a5726c8fd90ff23ec5af72b2d05d642a8f960c27
Rudolph <rudyjm3@yahoo.com>
2024-06-05 08:35:44 -0400

    Sort Function Fix:
    When any table header was clicked to be sorted if clicked a 2nd time it would not work. Would have to click a different header to trigger the function again.

--- assets/app.js
+++ assets/app.js
@@ -60,8 +60,8 @@ function sortTable(n) {
       } else {
          /* If no switching has been done AND the direction is "asc",
          set the direction to "desc" and run the while loop again. */
-         if (switchcount == 0 && dir == "asc") {
-            dir = "desc";
+         if (switchcount == 0 && dir == "desc") {
+            dir = "asc";
             switching = true;
          }
       }
