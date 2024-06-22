0fd1e9298dd072cc18b9ad29dcd631c033a98e42
Rudolph <rudyjm3@yahoo.com>
2024-06-22 00:41:44 -0400

    Added comment for live server database details

--- db-conn-info.php
+++ db-conn-info.php
@@ -5,4 +5,10 @@ $username = "root";
 $password = "";
 $dbname = "tool_log";
 
+// Live server connection details
+// $servername = "localhost";
+// $username = "u207292155_rudyjm3";
+// $password = "Matrix1645";
+// $dbname = "u207292155_toollog";
+
 ?>
